eefa9dea7894df089de9a4f2ef5b4b3894d031a9
peytonmandato <peyton.mandato@gmail.com>
2026-05-11 11:38:26 -0400

    card_swipe iteration 1

--- Cards_swipe/cards_swipe.js
+++ Cards_swipe/cards_swipe.js
@@ -4,7 +4,7 @@ var topIndex = 0;
 
 var cardWidth = 393;
 var distance = 0;
-drag = 0;
+var direction = 1;
 
 var isDragging = false;
 var startX = 0;
@@ -12,14 +12,14 @@ var startX = 0;
 //updates cards
 function update(){
     deck.forEach(function(card, index) {
+        let position;
         //finds the positino of each card in the stack and offsets it
-        let position = (index - topIndex + deck.length) % deck.length;
+        if (direction === 1) {position = (index - topIndex + deck.length) % deck.length;}
+        else {position = (topIndex - index + deck.length) % deck.length;}
         let offset = position * 8;
 
         //card follows cursor
-        if (position ===0){
-            offset += distance;
-        }
+        if (position ===0){offset += distance;}
 
     //stack the cards
     card.style.zIndex = deck.length - position;
@@ -31,15 +31,19 @@ function update(){
 //checks if card is being clicked and starts drag
 window.addEventListener("pointerdown", function(event) {
     isDragging = true;
+    var card = deck[topIndex];
+    card.classList.add("dragging");
     startX = event.clientX;  //updates var to its position actuelle
 });
 
 //checks if the clicker is moving, drags the top card
 window.addEventListener("pointermove", function(event) {
-    if (!isDragging) {
-        return;
-    }
+    if (!isDragging) {return;}
     distance = event.clientX - startX;
+
+    //check direction
+    if (distance>0){direction = -1;}
+    else {direction = 1;}
     update(); //displays accordingly
 });
 
@@ -50,34 +54,43 @@ window.addEventListener("pointerup", function(){
     isDragging = false;
     var done_swipe = 100; //sets the swipe threshold to 100px
     var card = deck[topIndex];
+    card.classList.remove("dragging");
 
     // checks if the user has swiped fully left or right
     if (distance < -done_swipe) {
-        //adding annimation from CSS
-        card.classList.add("swipeLeft");
+        //replacing animation
+        card.style.transform = "translate(-50%, -50%) translateX(-120vw)";
+        card.style.opacity = "0";
 
-        card.addEventListener("animationend", function(){
-            card.classList.remove("swipeLeft");
+        setTimeout(function(){
+            //reset
+            card.style.transform = "translate(-50%, -50%)";
+            card.style.opacity = "1";
+            
 
             topIndex +=1;
             if (topIndex >= deck.length) {topIndex = 0;}
         
             distance = 0;
             update();
-        }, {once: true}); //executes once animationend == true
+
+        }, 500); //waits 500 ms
     } 
     else if (distance > done_swipe) {
-        card.classList.add("swipeRight");
+        card.style.transform = "translate(-50%, -50%) translateX(120vw)";
+        card.style.opacity = "0";
 
-        card.addEventListener("animationend", function(){
-            card.classList.remove("swipeRight");
+        setTimeout(function(){
+            //reset
+            card.style.transform = "translate(-50%, -50%)";
+            card.style.opacity = "1";
 
             topIndex -=1;
             if (topIndex < 0){topIndex = deck.length -1;}
 
             distance = 0;
             update();
-        }, {once: true});  //executes once animationend == true
+        }, 500); 
     }
     else {    //snap back
         distance = 0;
