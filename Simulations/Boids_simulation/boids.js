24c96420edb378e47c78336fa3fb9d651060a335
peytonmandato <peyton.mandato@gmail.com>
2026-05-17 18:39:41 -0400

    boids simulation

---
+++ Simulations/Boids_simulation/boids.js
@@ -0,0 +1,120 @@
+class Boid {
+    constructor(){
+        this.position = createVector(random(width), random(height));
+        this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(2, 4));
+        this.acceleration = createVector(0,0);
+        this.maxForce = 0.2;
+        this.maxSpeed = speed_slider.value();
+    }
+
+    walls(){
+        if (this.position.y > height){this.position.y = 0;}
+        else if (this.position.y < 0) { this.position.y = height;}
+        if (this.position.x > width){this.position.x = 0;}
+        else if (this.position.x < 0) { this.position.x = width;}
+    }
+
+    // Aligns this boid with the average of the other boids in the given array (boids in the radius x)
+    align(boids){
+        //alignment.mult(alignment_slider.value());
+        let view = radius_slider.value();  //this is the radius that 'this' is seeing
+        let average = createVector();
+        let boids_near = 0;
+        for (let other of boids){ //loops over elements in boids
+            let d = Math.sqrt(((this.position.x - other.position.x)**2) + ((this.position.y - other.position.y)**2));
+            if (other != this && d < view){
+                average.add(other.velocity); //vector addition, not +=
+                boids_near +=1;
+            }
+        }
+        if (boids_near >0){
+            average.div(boids_near); //vector division, not /=
+            average.setMag(this.maxSpeed);
+            average.sub(this.velocity);
+            average.limit(this.maxForce); //caps the amount of force that can be applied
+        } //the average velocity is the boids desired velocity/the steering force
+        return average;
+    }
+
+    separation(boids){
+        let view = radius_slider.value();  //this is the radius that 'this' is seeing
+        let average = createVector();
+        let boids_near = 0;
+        for (let other of boids){ //loops over elements in boids
+            let d = Math.sqrt(((this.position.x - other.position.x)**2) + ((this.position.y - other.position.y)**2));
+            if (other != this && d < view){
+                let difference = p5.Vector.sub(this.position, other.position);
+                difference.div(d); //inversly proportional to the distance -> the farther the lower the pull/push
+                average.add(difference);
+                boids_near +=1;
+            }
+        }
+        if (boids_near >0){
+            average.div(boids_near); //vector division, not /=
+            average.setMag(this.maxSpeed);
+            average.sub(this.velocity);
+            average.limit(this.maxForce); //caps the amount of force that can be applied
+        } //the average velocity is the boids desired velocity/the steering force
+        return average;
+    }
+
+    //Copy of align code except matches position rather than velocity
+    cohesion(boids){
+        let view = radius_slider.value();  //this is the radius that 'this' is seeing
+        let average = createVector();
+        let boids_near = 0;
+        for (let other of boids){ //loops over elements in boids
+            let d = Math.sqrt(((this.position.x - other.position.x)**2) + ((this.position.y - other.position.y)**2));
+            if (other != this && d < view){
+                average.add(other.position); //vector addition, not +=
+                boids_near +=1;
+            }
+        }
+        if (boids_near >0){
+            average.div(boids_near); //vector division, not /=
+            average.sub(this.position); //subtracts the average velocity from this velocity
+            average.setMag(this.maxSpeed);
+            average.sub(this.velocity);
+            average.limit(this.maxForce); //caps the amount of force that can be applied
+        } //the average velocity is the boids desired velocity/the steering force
+        return average;
+    }
+
+    flock(num_boids){
+        let alignment = this.align(num_boids);
+        let cohesion = this.cohesion(num_boids);
+        let separation = this.separation(num_boids);
+
+// Scale them according to the slider
+        alignment.mult(alignment_slider.value());
+        cohesion.mult(cohesion_slider.value());
+        separation.mult(separation_slider.value());
+
+        //this.acceleration = alignment; //in a world where m=1, F=A
+        //this.acceleration = cohesion;
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
+        this.acceleration.add(separation);
+    }
+
+    update(){
+        this.position.add(this.velocity);
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+        this.acceleration.set(0,0); //acceleration shouldn't accumulate over time
+    }
+
+    show(){
+        fill(255);
+        stroke(255);
+
+        let angle = this.velocity.heading(); //returns direction angle in rad
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(angle);
+        triangle(6,0, -8,4, -8,-4);
+        pop();
+    }
+
+}
---
+++ Simulations/Boids_simulation/sketch_boids.js
@@ -0,0 +1,51 @@
+const school = [];
+
+let alignment_slider, cohesion_slider, separation_slider, radius_slider, speed_slider;
+
+function setup(){
+    createCanvas(640, 360);
+
+    let alignLabel = createP("Alignment");
+    alignLabel.addClass("slider-label");
+    alignment_slider = createSlider(0, 5, 1, 0.1); //lowest, largest, start value, increment
+    alignment_slider.addClass('slider');
+    alignment_slider.id('alignment');
+
+    let cohesionLabel = createP("Cohesion");
+    cohesionLabel.addClass("slider-label");
+    cohesion_slider = createSlider(0, 5, 1, 0.1);
+    cohesion_slider.addClass('slider');
+    cohesion_slider.id('cohesion');
+
+    let separateLabel = createP("Separation");
+    separateLabel.addClass("slider-label");
+    separation_slider = createSlider(0, 5, 1, 0.1);
+    separation_slider.addClass('slider');
+    separation_slider.id('separation');
+
+    let radiusLabel = createP("Radius");
+    radiusLabel.addClass("slider-label");
+    radius_slider = createSlider(1, 100, 50, 1);
+    radius_slider.addClass('slider');
+    radius_slider.id('radius');
+
+    let speedLabel = createP("Speed");
+    speedLabel.addClass("slider-label");
+    speed_slider = createSlider(1, 8, 4, 0.5);
+    speed_slider.addClass('slider');
+    speed_slider.id('radius');
+
+    for (let i = 0; i<100; i++){school.push(new Boid());}
+}
+
+function draw(){
+    background(50);
+
+    // for of loops over values, for in loops over indexes
+    for (let boid of school){ 
+        boid.walls(); //checks if it hits wall, loops around
+        boid.flock(school);
+        boid.update();
+        boid.show();
+    }
+}
